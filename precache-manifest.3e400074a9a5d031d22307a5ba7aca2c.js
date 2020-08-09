51802729e22e1c50b76991d13984d4a4fbc221fe
Shay Reichert <63343303+Kurtzs@users.noreply.github.com>
2020-08-09 15:00:34 +0200

    Updates

---
+++ precache-manifest.3e400074a9a5d031d22307a5ba7aca2c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2a6bed41cd2c64958420033a07f8c3",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "019097b5ecf8e19518cc",
+    "url": "/drum-machine/static/css/main.2130a7fa.chunk.css"
+  },
+  {
+    "revision": "4bad4aac821f60166e6c",
+    "url": "/drum-machine/static/js/2.4fa3396d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.4fa3396d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019097b5ecf8e19518cc",
+    "url": "/drum-machine/static/js/main.52a43421.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  },
+  {
+    "revision": "d198edc721d809b7e8f7dd7a6c09392f",
+    "url": "/drum-machine/static/media/8080Dmajor.d198edc7.mp3"
+  },
+  {
+    "revision": "010e8032e8ccda5f806aa563c3f5a625",
+    "url": "/drum-machine/static/media/808AMajor.010e8032.mp3"
+  },
+  {
+    "revision": "671f9e5e1fda28dc91dc4717bd281b6b",
+    "url": "/drum-machine/static/media/808Dminor.671f9e5e.mp3"
+  },
+  {
+    "revision": "55bb7cbe2d11ac5f882fb244618c10d1",
+    "url": "/drum-machine/static/media/808F.55bb7cbe.mp3"
+  },
+  {
+    "revision": "abda28085ba85c79fbad0b1b1deb12ee",
+    "url": "/drum-machine/static/media/BoneBreaking.abda2808.mp3"
+  },
+  {
+    "revision": "437e4b9295f401411fa9634fab598c34",
+    "url": "/drum-machine/static/media/apple.437e4b92.mp3"
+  },
+  {
+    "revision": "c6445583b6c4d52710ec4c9d14c01597",
+    "url": "/drum-machine/static/media/charley01.c6445583.mp3"
+  },
+  {
+    "revision": "cdb0bbe1b27a89925d099c60e3f40507",
+    "url": "/drum-machine/static/media/charleyOpen01.cdb0bbe1.mp3"
+  },
+  {
+    "revision": "eebe4570563289b44f4e06ed153b5b77",
+    "url": "/drum-machine/static/media/fireBow.eebe4570.mp3"
+  },
+  {
+    "revision": "3a73aace34034f55f2edd4d9f6df3146",
+    "url": "/drum-machine/static/media/kick01.3a73aace.mp3"
+  },
+  {
+    "revision": "462f8ffd15b01ad250d8fb20cb32da92",
+    "url": "/drum-machine/static/media/lightSwith.462f8ffd.mp3"
+  },
+  {
+    "revision": "15425b81bc403086bc83a0319fb03964",
+    "url": "/drum-machine/static/media/marbles.15425b81.mp3"
+  },
+  {
+    "revision": "bd1c52aec9f521245a4cdec8ce815b20",
+    "url": "/drum-machine/static/media/neckSnap.bd1c52ae.mp3"
+  },
+  {
+    "revision": "3a254e16164a80efdc0618a37aa625cf",
+    "url": "/drum-machine/static/media/pinDropping.3a254e16.mp3"
+  },
+  {
+    "revision": "f4b95df95e6d00c9ffaea7beba286e09",
+    "url": "/drum-machine/static/media/shortCircuit.f4b95df9.mp3"
+  },
+  {
+    "revision": "6b0b1cb4c419968d57fe5c072010bded",
+    "url": "/drum-machine/static/media/snare01.6b0b1cb4.mp3"
+  },
+  {
+    "revision": "7bbdc8cf40a825539bd05baf1b349f03",
+    "url": "/drum-machine/static/media/stringTension.7bbdc8cf.mp3"
+  },
+  {
+    "revision": "483725042a0c9f5dfc3ee36c91b5f8d0",
+    "url": "/drum-machine/static/media/waterBalloon.48372504.mp3"
+  }
+]);
